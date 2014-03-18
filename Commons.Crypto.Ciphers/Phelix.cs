1759083ec504f701fcd8fbce2533cfaad74f2607
Rafael Teixeira <monoman@gmail.com>
2014-03-18 16:26:06 -0300

    Adding unit tests
    API also changed

--- Commons.Crypto.Ciphers/Phelix.cs
+++ Commons.Crypto.Ciphers/Phelix.cs
@@ -8,26 +8,48 @@ namespace Commons.Crypto.Ciphers
 
 	public class Phelix
 	{
-		private byte[] key;
+		public const int NONCE_SIZE = 128;  /* size of nonce        in bits */
+		public const int MAC_SIZE = 128;    /* size of full mac tag in bits */
+		public const int KEY_SIZE = 256;    /* size of full key     in bits */
+
+		private readonly byte[] _key;
 
 		public Phelix(byte[] key)
 		{
-			this.key = key;
+			if (key == null)
+				throw new ArgumentNullException("key");
+			if (key.Length != (KEY_SIZE / 8))
+				throw new ArgumentException("The key MUST have " + KEY_SIZE + " bits!", "key");
+			_key = key;
 		}
 
-		public EncryptedWithTag Encrypt(byte[] plaintext, Nonce nonce)
+		public byte[] Encrypt(byte[] plaintext, byte[] nonce, out byte[] mac)
 		{
-			return new EncryptedWithTag(null, 0);
+			ConstrainNonce(nonce);
+			mac = new byte[MAC_SIZE / 8];
+			return plaintext;
 		}
 
-		public EncryptedWithTag Encrypt(byte[] plaintext, Nonce nonce, int headersLength)
+		public byte[] Encrypt(byte[] plaintext, byte[] nonce, int headersLength, out byte[] mac)
 		{
-			return new EncryptedWithTag(null, 0);
+			ConstrainNonce(nonce);
+			mac = new byte[MAC_SIZE / 8];
+			return plaintext;
 		}
 
-		public byte[] Decrypt(EncryptedWithTag encripted)
+		public byte[] Decrypt(byte[] ciphertext, byte[] nonce, out byte[] mac)
 		{
-			return new byte[0];
+			ConstrainNonce(nonce);
+			mac = new byte[MAC_SIZE / 8];
+			return ciphertext;
+		}
+
+		private static void ConstrainNonce(byte[] nonce)
+		{
+			if (nonce == null)
+				throw new ArgumentNullException("nonce");
+			if (nonce.Length != (KEY_SIZE / 8))
+				throw new ArgumentException("The nonce MUST have " + NONCE_SIZE + " bits!", "nonce");
 		}
 
 	}
---
+++ test/Unit.Commons.Crypto.Phelix/Commons.Crypto.Ciphers/TestPhelix.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using Commons.Crypto.Ciphers;
+
+namespace Unit.Commons.Crypto.Ciphers
+{
+
+	[TestFixture]
+	public class TestPhelix
+	{
+		[Test]
+		public void ConstructWithNullKeyThrowsArgumentNullException()
+		{
+			Assert.Catch<ArgumentNullException>(() => new Phelix(null));
+		}
+
+		[Test]
+		public void ConstructWithShorterKeyThrowsArgumentException()
+		{
+			Assert.Catch<ArgumentException>(() => new Phelix(new byte[4] { 8, 16, 24, 32 }));
+		}
+
+		[Test]
+		public void ConstructWithLongerKeyThrowsArgumentException()
+		{
+			Assert.Catch<ArgumentException>(() => new Phelix(new byte[33] { 8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104, 112, 120, 128, 
+				136, 144, 152, 160, 168, 176, 184, 192, 200, 208, 216, 224, 232, 240, 248, 0, 8 }));
+		}
+
+		[Test]
+		public void ConstructWithNiceKey()
+		{
+			Assert.NotNull(new Phelix(new byte[32] { 8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104, 112, 120, 128,
+													136, 144, 152, 160, 168, 176, 184, 192, 200, 208, 216, 224, 232, 240, 248, 0}));
+		}
+	}
+}
