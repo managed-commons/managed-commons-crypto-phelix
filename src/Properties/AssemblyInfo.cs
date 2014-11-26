fd8a24b1fb49859e27b1c39d7f24a6cbd4005f6e
Rafael Teixeira <monoman@gmail.com>
2014-11-26 17:29:29 -0200

    Fix AssemblyInfo

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -2,35 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Managed.Commons.Phelix")]
+[assembly: AssemblyTitle("Managed.Commons.Crypto.Phelix")]
 [assembly: AssemblyDescription("Phelix Cipher/MAC")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Managed Commons Team")]
-[assembly: AssemblyProduct("Managed.Commons.Phelix")]
+[assembly: AssemblyProduct("Managed.Commons.Crypto.Phelix")]
 [assembly: AssemblyCopyright("Copyright © Managed Commons Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6d353780-6f73-44ef-9a1d-7bbe98725c61")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
